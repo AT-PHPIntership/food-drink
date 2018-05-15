ab7a6fd101c56c04796e96371cd4dd30feeaa025
thanhthai <thai tran>
2018-05-15 12:53:44 +0700

    add templates admin home

---
+++ resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,34 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b></b>
+    </div>
+    <strong>Copyright &copy; 2018 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    reserved.
+  </footer>
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="bower_components/admin-lte/dist/js/adminlte.min.js"></script>
+<!-- Sparkline -->
+<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<!-- jvectormap  -->
+<script src="bower_components/admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="bower_components/admin-lte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<!-- SlimScroll -->
+<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- ChartJS -->
+<script src="bower_components/chart.js/Chart.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="bower_components/admin-lte/dist/js/pages/dashboard2.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="bower_components/admin-lte/dist/js/demo.js"></script>
+</body>
+</html>
---
+++ resources/views/admin/layout/master.blade.php
@@ -0,0 +1,4 @@
+@include('admin.layout.header')
+@include('admin.layout.sidebar')
+@yield('content')
+@include('admin.layout.footer')
