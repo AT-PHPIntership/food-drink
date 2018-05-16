538b50f51663777e27be09fb196cc8550188332e
dung le <dung.le@asiantech.vn>
2018-05-16 14:37:41 +0700

    fix-func-down

--- database/migrations/2018_05_16_021651_default_role_to_users_table.php
+++ database/migrations/2018_05_16_021651_default_role_to_users_table.php
@@ -26,7 +26,7 @@ class DefaultRoleToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
         });
     }
 }
