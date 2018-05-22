6ccad893eebebd7096b4f3197d332b643aa4d415
at-thaitran <thai.tran@asiantech.vn>
2018-05-22 12:00:54 +0700

    write unit test edit user

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,98 @@
+<?php
+use Faker\Generator as Faker;
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+$factory->define(App\Category::class, function (Faker $faker) {
+    // And now let's generate a few dozen users for our app:
+        return([
+            'name' => $faker->name,
+            'parent_id' => App\Category::all()->random()->id,
+        ]);
+});
+$factory->define(App\Images::class, function (Faker $faker) {
+    return([
+        'image' => config('image.images_product'),
+        'product_id' => $faker->randomElement(App\Product::pluck('id')->toArray()),
+    ]);
+});
+$factory->define(App\OrderDetail::class, function (Faker $faker) {
+    return [
+        'order_id' => $faker->randomElement(App\Order::pluck('id')->toArray()),
+        'product_id' => $faker->randomElement(App\Product::pluck('id')->toArray()),
+        'quantity' => $faker->numberBetween(1, 5),
+        'price' => $faker->numberBetween(10, 200),
+        'preview' => $faker->randomElement(App\Product::pluck('preview')->toArray()),
+        'address' => $faker->address,
+        'name_product' => $faker->randomElement(App\Product::pluck('name')->toArray()),
+        'image' => config('image.images_product'),
+        'created_at' => $faker->dateTime,
+        'updated_at' => $faker->dateTime,
+    ];
+});
+$factory->define(App\Post::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->randomElement(App\User::pluck('id')->toArray()),
+        'product_id' => $faker->randomElement(App\Product::pluck('id')->toArray()),
+        'content' => $faker->text,
+        'rate' => $faker->numberBetween(0, 5),
+        'type' => random_int(1, 2),
+        'created_at' => $faker->dateTime,
+        'updated_at' => $faker->dateTime,
+    ];
+});
+$factory->define(App\Order::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->randomElement(App\User::pluck('id')->toArray()),
+        'total' => 1000,
+        'status' => random_int(1, 3),
+        'created_at' => $faker->dateTime,
+        'updated_at' => $faker->dateTime,
+    ];
+});
+$factory->define(App\Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'price' => $faker->numberBetween(10, 200),
+        'quantily' =>$faker->numberBetween(0, 15),
+        'category_id' => $faker->randomElement(Category::pluck('id')->toArray()),
+        'preview' => $faker->text,
+        'description' => $faker->text,
+        'avg_rate' => $faker->randomFloat(1, 1, 5),
+        'sum_rate' => $faker->numberBetween(1, 20),
+        'total_rate' =>$faker->numberBetween(10, 80),
+        'created_at' => $faker->dateTime,
+        'updated_at' => $faker->dateTime,
+    ];
+});
+$factory->define(App\UserInfo::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->unique()->randomElement(App\User::pluck('id')->toArray()),
+        'address' => $faker->address,
+        'phone' => $faker->phoneNumber,
+        'avatar' => config('image.images_user'),
+    ];
+});
+$factory->define(App\User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'password' => bcrypt(config('auth.defaults.passwords')),
+        'role' => 0,
+    ];
+});
+$factory->defineAs(App\User::class, 'admin', function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => bcrypt(config('auth.defaults.passwords')), // secret
+        'role' => 1,
+    ];
+ });
---
+++ tests/Browser/Pages/Backend/User/EditUserTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Browser\Pages\Backend\User;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
+use App\UserInfo;
+
+class EditUserTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    const NUMBER_RECORD_CREATE = 5;
+    const NUMBER_RECORD_FIND = 5;
+
+    public function setUp()
+    {
+        parent::setUp();
+        factory(User::class, self::NUMBER_RECORD_CREATE)->create();
+        factory(UserInfo::class, self::NUMBER_RECORD_CREATE)->create();
+    }
+
+    /**
+     * Test Users Edit Success.
+     *
+     * @return void
+     */
+    public function testEditUser()
+    {
+        $user = User::find(self::NUMBER_RECORD_FIND);
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('admin/user/'.$user->id.'/edit')
+                    ->assertSee('Edit Form');
+        });
+    }
+
+    
+
+    /**
+    * Test Users Edit Success.
+    *
+    * @return void
+    */
+    public function testEditUserSuccess()
+    {
+        $user = User::find(self::NUMBER_RECORD_FIND);
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/admin/user/'.$user->id.'/edit')
+                    ->resize(900,1000)
+                    ->type('name', $user->name)
+                    ->press('submit')
+                    ->assertSee('Successfully Updated User')
+                    ->assertPathIs('/admin/user');
+        });
+    }
+
+    /**
+     * Test Validation Update User.
+     *
+     * @return void
+     */
+     public function testValidationUpdateUser()
+     {
+        $user = User::find(self::NUMBER_RECORD_FIND);
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/admin/user/'.$user->id.'/edit')
+                    ->type('name', '')
+                    ->type('phone', '(873) 396-4030(873) 396-4030(873) 396-4030(873) 396-4030(873) 396-4030(873) 396-4030')
+                    ->type('address', '4361 Jayce Summit Apt. 286North Mariobury')
+                    ->press('submit')
+                    ->assertPathIs('/admin/user/'.$user->id.'/edit')
+                    ->assertSee('The name field is required.')
+                    ->assertSee('The phone may not be greater than 50 characters.');
+        });
+     }
+ 
+}
