32b4ba992e58a7eba62c527b3b6e9caa4bd4e0b7
at-sonle <son.le@asiantech.vn>
2018-05-23 10:42:18 +0700

    layout list category and get category

---
+++ resources/lang/en/category.php
@@ -0,0 +1,18 @@
+<?php
+    return [
+        'admin' => [
+            'index' => [
+                'title' => 'Show List Categories',
+                'list_category' => 'List Category',
+                'categories' => 'Categories',
+                'new_category' => 'New Category',
+                'show_product' => 'Show list products',
+                'search' => 'Search',
+                'id' => 'ID',
+                'name' => 'Name',
+                'parent' => 'Parent',
+                'action' => 'Action',
+            ]
+        ]
+    ];
+    
