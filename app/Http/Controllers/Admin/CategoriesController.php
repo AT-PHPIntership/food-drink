021b155de67c339c63f17ad3752fb9407402668c
at-sonle <son.le@asiantech.vn>
2018-05-25 11:43:47 +0700

    validation category

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Category;
+use Illuminate\Http\Request\CategoryRequest;
 
 class CategoriesController extends Controller
 {
