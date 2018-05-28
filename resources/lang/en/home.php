a3168ee638f754f501b46c629c5311d1d63183f9
at-thaitran <thai.tran@asiantech.vn>
2018-05-28 23:01:56 +0700

    fix layout home admin

---
+++ resources/lang/en/home.php
@@ -0,0 +1,15 @@
+<?php
+    return [
+        'total_product' => 'Total Products',
+        'total_product_orderd' => 'Total Product Orderd',
+        'total_revenue' => 'Total Revenue',
+        'total_member' => 'Total Members',
+        'in_week' => 'In Week',
+        'in_month' => 'In Month',
+        'order_id' => 'Order ID',
+        'email' => 'Email',
+        'status' => 'Status',
+        'time' => 'Times',
+        'view_all_order' => 'View All Orders',
+        'latest_order' => 'Latest Orders',
+    ];
