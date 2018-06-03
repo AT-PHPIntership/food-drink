06195923e73c2b1acaeb266e88aa1c221337a4f9
at-thaitran <thai.tran@asiantech.vn>
2018-06-03 22:46:14 +0700

    search order

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -8,13 +8,25 @@ use App\Order;
 class OrdersController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+    * Display a listing of the resource.
+    *
+    * @param \Illuminate\Http\Request $request request
+    *
+    * @return \Illuminate\Http\Response
     */
-    public function index()
+    public function index(Request $request)
     {
-        $orders = Order::with('user')->paginate(config('define.number_page_products'));
+        $orders = Order::with('user');
+        $search = $request->search;
+        if ($search != '') {
+            $orders = $orders->whereHas('user', function ($query) use ($search) {
+                return $query->where('name', 'Like', "%$search%")
+                            ->orWhere("email", 'Like', "%$search%");
+            });
+            $orders = $orders->paginate(config('define.number_page_products'))->appends(['search' => $search]);
+        } else {
+            $orders = $orders->paginate(config('define.number_page_products'));
+        }
         return view('admin.order.index', compact('orders'));
     }
 }
