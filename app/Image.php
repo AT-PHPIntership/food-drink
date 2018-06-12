9bbe3632939d6ab29c6aded5edc9341540518b9b
at-sonle <son.le@asiantech.vn>
2018-06-12 10:31:52 +0700

    add path image

--- app/Image.php
+++ app/Image.php
@@ -34,4 +34,16 @@ class Image extends Model
     {
         return asset(config('define.images_path_products') . $this->image);
     }
+
+    public function toArray()
+   {
+       return [
+           'id' => $this->id,
+           'product_id' => $this->product_id,
+           'image' => $this->getImageUrlAttribute(),
+           'created_at' => $this->created_at,
+           'updated_at' => $this->updated_at,
+           'deleted_at' => $this->deleted_at,
+       ];
+   }
 }
