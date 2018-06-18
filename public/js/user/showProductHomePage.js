68136426c7659a6d5a6b654bbdd45eeb48520c0c
at-thaitran <thai.tran@asiantech.vn>
2018-06-18 19:45:13 +0700

    Remove duplicate code

--- public/js/user/showProductHomePage.js
+++ public/js/user/showProductHomePage.js
@@ -1,57 +1,44 @@
 var idCategoryFood = 1;
 var idCategoryDrink = 2;
 var limit = 8;
-var id = '';
+var rate = 0;
 
-//Top 8 New Product
-$('document').ready(function () {
+$(document).ready(function () {
+  //Top 8 New Product
   $.get('api/products?sort=created_at&order=desc&limit=' + limit, function(response){
-    id = 'new-product';
-    appendHtml(id, response);
+    appendHtml('new-product', response);
   });
-});
-
-//Top 8 Rate Product
-$('document').ready(function () {
+  //Top 8 Rate Product
   $.get('api/products?sort=avg_rate&order=desc&limit=' + limit, function(response){
-    id = 'rate-product';
-    appendHtml(id, response);
+    appendHtml('rate-product', response);
   });
-});
-
-//show product by category food
-$('document').ready(function () {
+  //show product by category food
   $.get('/api/products?category=' + idCategoryFood, function(response){
-    id = 'food-product';
-    appendHtml(id, response);
+    appendHtml('food-product', response);
   });
-});
-
-//show product by category drink
-$('document').ready(function () {
+  //show product by category drink
   $.get('/api/products?category=' + idCategoryDrink, function(response){
-    id = 'drink-product';
-    appendHtml(id, response);
+    appendHtml('drink-product', response);
   });
 });
 
 //append Html
 function appendHtml(id, response) {
   response.data.data.forEach(element => {
+    var stars = '';
     img_url = 'https://image.ibb.co/dqd4QJ/default_product.jpg';
     if (typeof element.images[0] !== 'undefined') {
       img_url = element.images[0].image_url;
     }
-    let stars = '';
-    let rate = Math.round(element.avg_rate);
+    rate = Math.round(element.avg_rate);
     for (i = 1; i <= 5 ; i++) {
       if (i <= rate) {
         stars += '<i class="fa fa-star"></i>';
       } else {
-        stars += '<i class="fa fa-star-o"></i>'
+        stars += '<i class="fa fa-star-o"></i>';
       }
     }
-    $('#'+id+'').append(
+    $('#'+id).append(
       '<li class="item col-lg-3 col-md-3 col-sm-6 col-xs-6 ">\
         <div class="product-item">\
           <div class="item-inner">\
