d4e070632070b8e4f0c5710ff94a855d38ca70d2
at-thaitran <thai.tran@asiantech.vn>
2018-06-19 13:53:38 +0700

    add to cart

---
+++ public/js/user/addCart.js
@@ -0,0 +1,43 @@
+var item = [];
+var cartProduct = [];
+var n = 0;
+
+function numberItem() {
+  if (localStorage.count) {
+    document.getElementById('number-item').innerHTML = localStorage.count;
+  }
+}
+numberItem();
+
+function addCart(idProduct, nameProduct, priceProduct, quantityProduct, imgProduct) {
+  var exists = false;
+  item = {
+    id: idProduct,
+    name: nameProduct,
+    price: priceProduct,
+    quantity: quantityProduct,
+    img_url: imgProduct,
+    count: 1
+  };
+  if (typeof(Storage) !== 'undefined') {
+    if (localStorage.carts) {
+      cartProduct = JSON.parse(localStorage.carts);
+      $.each(cartProduct, function(index, value) {
+        if(value.id === item.id) {
+          value.count++;
+          exists = true;
+          return false;
+        }
+      });
+    }
+  }
+  if(!exists) {
+    cartProduct.push(item);
+    localStorage.setItem('carts', JSON.stringify(cartProduct));
+    getcarts = JSON.parse(localStorage.carts);
+    n = getcarts.length;
+    localStorage.setItem('count', n);
+  }
+  numberItem();
+  localStorage.setItem('carts', JSON.stringify(cartProduct));
+}
--- resources/views/user/layout/script.blade.php
+++ resources/views/user/layout/script.blade.php
@@ -5,6 +5,7 @@
 <script type="text/javascript" src="{{ asset('frontend/js/bootstrap.min.js')}} "></script> 
 
 <script src="{{ asset('js/messages.js') }}"></script>
+<script src="{{ asset('js/user/addCart.js') }}"></script>
 
 <!-- owl.carousel.min js --> 
 <script type="text/javascript" src="{{ asset('frontend/js/owl.carousel.min.js')}} "></script> 
