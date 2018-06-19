6fce5b62d70098f5a43c16b9507f3d8d43c4ca44
at-sonle <son.le@asiantech.vn>
2018-06-19 16:07:29 +0700

    layout product post

---
+++ resources/views/user/post/postOfProduct.blade.php
@@ -0,0 +1,26 @@
+<div class="col-sm-12">
+		<h5 class="entry-title">
+			<a href="single_post.html">
+				Aliquam Et Metus Pharetra, Bibendum Massa
+			</a>
+		</h5>
+		<div class="entry-meta-data">
+			<span class="author">
+				<i class="fa fa-user"></i>&nbsp; by:Admin |
+			</span>
+			<span class="date">
+				<i class="fa fa-calendar"></i>&nbsp; 2015-08-05
+			</span> 
+		</div>
+		<div class="rating">
+			<i class="fa fa-star"></i>
+			<i class="fa fa-star"></i>
+			<i class="fa fa-star"></i>
+			<i class="fa fa-star-o"></i>
+			<i class="fa fa-star-o">
+			</i>&nbsp; <span>(5 votes)</span>
+		</div>
+		<div class="entry-excerpt"> 
+			Cras Pretium Arcu Ex.
+		</div>
+</div>
