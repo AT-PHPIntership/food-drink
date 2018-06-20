40459cf33c4bccb55f3d42fe1a2560b07bea31f4
at-sonle <son.le@asiantech.vn>
2018-06-20 11:18:31 +0700

    g use api post of product

--- resources/views/user/post/postOfProduct.blade.php
+++ resources/views/user/post/postOfProduct.blade.php
@@ -1,25 +1,3 @@
-<div class="col-sm-12">
-    <h5 class="entry-title">
-      <a href="single_post.html">
-      </a>
-    </h5>
-    <div class="entry-meta-data">
-      <span class="author">
-        <i class="fa fa-user">&nbsp; by:Admin |</i>
-      </span>
-      <span class="date">
-        <i class="fa fa-calendar">&nbsp; 2015-08-05</i>
-      </span> 
-    </div>
-    <div class="rating">
-      <i class="fa fa-star"></i>
-      <i class="fa fa-star"></i>
-      <i class="fa fa-star"></i>
-      <i class="fa fa-star-o"></i>
-      <i class="fa fa-star-o">
-      </i>&nbsp; <span>(5 votes)</span>
-    </div>
-    <div class="entry-excerpt"> 
-      Cras Pretium Arcu Ex.
-    </div>
+<div id="contain-posts">
+
 </div>
