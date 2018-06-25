4c5e0d405bb12c5ab92a8c89bc50729f186ceb77
at-thaitran <thai.tran@asiantech.vn>
2018-06-25 15:43:38 +0700

    fix cmt tab

--- public/js/user/addPost.js
+++ public/js/user/addPost.js
@@ -8,42 +8,39 @@ $(document).ready(function() {
   });
   $('#add-review').click(function(event) {
     event.preventDefault();
-		submitPost(pathName, TYPE_REVIEW);
+    submitPost(pathName, TYPE_REVIEW);
   });
 });
 
 function submitPost(pathName, TYPE) {
-
   $.ajax({
-		url: '/api' + pathName + '/posts',
-		type: 'POST',
-		headers: {
-			'Accept': 'application/json',
-			'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-		},
-		data: {
-			type: TYPE,
-			rate: $('.reviews-table [name="rate"]:radio:checked').val(),
-			content: $('#content-post' + TYPE).val(),
-		},
-		success: function(response) {
-			$('.alert-post'+ TYPE).show();
-		},
-		error: function(response) {
-			console.log(response.responseJSON.error);
-			
-			errorMessage = response.responseJSON.message + '<br/>';
-			if (response.responseJSON.errors) {
-				errors = Object.keys(response.responseJSON.errors);
-				errors.forEach(error => {
-					errorMessage += response.responseJSON.errors[error] + '<br/>';
-				});
-			}
-			if (response.responseJSON.code) {
-				errorMessage = Lang.get('product.user.detail.purchase');
-			}
-			$('.reviews-content-right .alert-danger').html(errorMessage);
-			$('.reviews-content-right .danger'+ TYPE).show();
-		}
+    url: '/api' + pathName + '/posts',
+    type: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+    },
+    data: {
+      type: TYPE,
+      rate: $('.reviews-table [name="rate"]:radio:checked').val(),
+      content: $('#content-post' + TYPE).val(),
+    },
+    success: function(response) {
+      $('.alert-post'+ TYPE).show();
+    },
+    error: function(response) {
+      errorMessage = response.responseJSON.message + '<br/>';
+      if (response.responseJSON.errors) {
+        errors = Object.keys(response.responseJSON.errors);
+        errors.forEach(error => {
+          errorMessage += response.responseJSON.errors[error] + '<br/>';
+        });
+      }
+      if (response.responseJSON.code) {
+        errorMessage = Lang.get('product.user.detail.purchase');
+      }
+      $('.reviews-content-right .alert-danger').html(errorMessage);
+      $('.reviews-content-right .danger'+ TYPE).show();
+    }
   });
 }
