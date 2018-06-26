b6d1632743813eef15e685adb16eb6ed51bfa7bf
dung le <dung.le@asiantech.vn>
2018-06-26 11:29:41 +0700

    layout product detail

--- resources/views/user/product/show.blade.php
+++ resources/views/user/product/show.blade.php
@@ -4,6 +4,46 @@
 <div class="main-container col1-layout">
   <div class="container">
     <div class="row">
+      <div class="col-main">
+        <div class="product-view-area">
+          <div class="product-big-image col-xs-12 col-sm-5 col-lg-5 col-md-5">
+            <div class="large-image"> 
+              <a href="images/products/img03.jpg" class="cloud-zoom" id="zoom1" rel="useWrapper: false, adjustY:0, adjustX:20"> 
+                <img class="zoom-img" src="{{ asset('images/products/default-product.jpg') }}" alt="products"> 
+              </a> 
+            </div>     
+          </div>
+          <div class="col-xs-12 col-sm-7 col-lg-7 col-md-7 product-details-area">
+              <div class="product-name">
+                <h1>Lorem Ipsum is simply</h1>
+              </div>
+              <div class="price-box">
+                <p class="special-price"> <span class="price-label">Special Price</span> <span class="price"> $329.99 </span> </p>
+              </div>
+              <div class="ratings">
+                <div class="rating"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star-o"></i> <i class="fa fa-star-o"></i> </div>
+                <p class="rating-links"> <span class="separator">Rate</span> </p>
+              </div>
+              <div class="short-description">
+                <h2>Quick Overview</h2>
+                <p>Accusamus repellat quo commodi earum quia quos. Qui recusandae porro incidunt aspernatur repellat eveniet recusandae odio. Dolor esse consequuntur nobis laudantium qui et.</p>
+              </div>
+              <div class="product-variation">
+                <form action="#" method="post">
+                  <div class="cart-plus-minus">
+                    <label for="qty">Quantity:</label>
+                    <div class="numbers-row">
+                      <div onClick="var result = document.getElementById('qty'); var qty = result.value; if( !isNaN( qty ) &amp;&amp; qty &gt; 0 ) result.value--;return false;" class="dec qtybutton"><i class="fa fa-minus">&nbsp;</i></div>
+                      <input type="text" class="qty" title="Qty" value="1" maxlength="12" id="qty" name="qty">
+                      <div onClick="var result = document.getElementById('qty'); var qty = result.value; if( !isNaN( qty )) result.value++;return false;" class="inc qtybutton"><i class="fa fa-plus">&nbsp;</i></div>
+                    </div>
+                  </div>
+                  <button class="button pro-add-to-cart" title="Add to Cart" type="button"><span><i class="fa fa-shopping-cart"></i> Add to Cart</span></button>
+                </form>
+              </div>
+          </div>
+        </div>
+      </div>
       <div class="product-overview-tab">
         <div class="container">
           <div class="row">
@@ -17,20 +57,7 @@
                 <div id="productTabContent" class="tab-content">
                   <div class="tab-pane fade in active" id="description">
                     <div class="std">
-                      <p>Proin lectus ipsum, gravida et mattis vulputate, 
-                        tristique ut lectus. Sed et lorem nunc. Vestibulum ante ipsum primis in 
-                        faucibus orci luctus et ultrices posuere cubilia Curae; Aenean eleifend 
-                        laoreet congue. Vivamus adipiscing nisl ut dolor dignissim semper. Nulla
-                        luctus malesuada tincidunt. Nunc facilisis sagittis ullamcorper. Proin 
-                        lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et 
-                        lorem nunc. Vestibulum ante ipsum primis in faucibus orci luctus et 
-                        ultrices posuere cubilia Curae; Aenean eleifend laoreet congue. Vivamus 
-                        adipiscing nisl ut dolor dignissim semper. Nulla luctus malesuada 
-                        tincidunt. Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, 
-                        gravida et mattis vulputate, tristique ut lectus. Sed et lorem nunc. 
-                        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
-                        cubilia Curae; Aenean eleifend laoreet congue. Vivamus adipiscing nisl 
-                        ut dolor dignissim semper. Nulla luctus malesuada tincidunt.</p>
+                      <p></p>
                     </div>
                   </div>
                   <!-- review -->
