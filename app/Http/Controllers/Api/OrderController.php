e1b2172c82b026365e1fee523b992a95f200e7e5
at-thaitran <thai.tran@asiantech.vn>
2018-06-28 21:33:03 +0700

    fix paginate

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -9,18 +9,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 use App\Order;
 use PHPUnit\Framework\MockObject\Stub\Exception;
+use App\Http\Requests\Api\SortOrderRequest;
 
 class OrderController extends ApiController
 {
     /**
      * Display a listing of the resource.
      *
+     * @param \Illuminate\Http\Request $request request
+     *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(SortOrderRequest $request)
     {
         $user = Auth::user();
-        $orders = Order::where('user_id', $user->id)->sortable()->get();
-        return $this->showAll($orders, Response::HTTP_OK);
+        $orders = Order::where('user_id', $user->id)->sortable()->paginate($request->limit);
+        $orders->appends(request()->query());
+        return $this->successResponse($orders, Response::HTTP_OK);
     }
 }
