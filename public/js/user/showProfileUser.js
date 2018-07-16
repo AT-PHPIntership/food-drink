0ea1222d5bbc9be894a903497bfb7a89f0f3d0b7
at-thaitran <thai.tran@asiantech.vn>
2018-07-16 09:21:55 +0700

    edit layout profile

--- public/js/user/showProfileUser.js
+++ public/js/user/showProfileUser.js
@@ -8,6 +8,8 @@ $( document ).ready(function() {
     url: '/api/profile',
     headers: { 'authorization': 'Bearer '+ localStorage.getItem('access_token') },
     success: function (response){
+      console.log(response);
+      
       $("#user-avatar").attr('src', response.data.user.user_info.avatar_url);
       $("#user-name").html(response.data.user.name);
       $("#user-address").html(response.data.user.user_info.address);
--- resources/views/user/profile/userProfile.blade.php
+++ resources/views/user/profile/userProfile.blade.php
@@ -1,7 +1,74 @@
 @extends('user.layout.master')
 @section('title', __('home.user.title'))
 @section('content')
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
 <div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          <h3 class="panel-title">Sheena Shrestha</h3>
+        </div>
+        <div class="panel-body">
+          <div class="row">
+            <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="http://babyinfoforyou.com/wp-content/uploads/2014/10/avatar-300x300.png" class="img-circle img-responsive"> </div>
+            <div class=" col-md-9 col-lg-9 "> 
+              <table class="table table-user-information">
+                <tbody>
+                  <tr>
+                    <td>Department:</td>
+                    <td>Programming</td>
+                  </tr>
+                  <tr>
+                    <td>Hire date:</td>
+                    <td>06/23/2013</td>
+                  </tr>
+                  <tr>
+                    <td>Date of Birth</td>
+                    <td>01/24/1988</td>
+                  </tr>
+                  <tr>
+                    <tr>
+                      <td>Gender</td>
+                      <td>Female</td>
+                    </tr>
+                    <tr>
+                      <td>Home Address</td>
+                      <td>Kathmandu,Nepal</td>
+                    </tr>
+                    <tr>
+                      <td>Email</td>
+                      <td><a href="mailto:info@support.com">info@support.com</a></td>
+                    </tr>
+                    <tr>
+                      <td>Phone Number</td>
+                      <td>
+                        <p>123-4567-890(Landline)</p>
+                        <p>555-4567-890(Mobile)</p>
+                      </td>
+                    </tr>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="panel-footer">
+          <a data-original-title="Broadcast Message" data-toggle="tooltip" type="button" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-envelope"></i></a>
+          <span class="pull-right">
+            <a href="edit.html" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
+            <a data-original-title="Remove this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- <div class="container">
   <div class="row">
     <div class="col-xs-12 col-sm-6 col-md-6">
       <div class="well well-sm">
@@ -30,5 +97,5 @@
 </div>
 @endsection
 @section('scripts')
-  <script type="text/javascript" src="{{ asset('js/user/showProfileUser.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/user/showProfileUser.js') }}"></script> -->
 @endsection
